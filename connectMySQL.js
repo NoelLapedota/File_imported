d2668b0e40e538da65ec4589fdbf719d41c00715
Mrleon123 <Mrleon@hotmail.it>
2022-11-19 15:35:27 +0100

    first commit

--- connectMySQL.js
+++ connectMySQL.js
@@ -9,19 +9,7 @@ async function db() {
   database: "fileImportDb2",
   });
 }
-// password: "",
-// database: "fileImportDb2",
-  // host: process.env.HOST,
-  // user: process.env.USER,
-  // password: process.env.PASSWORD,
-  // database: process.env.DATABASE,
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
 
-//   console.log("connected on mysql with ID " + connection.threadId);
-// });
+
 
 module.exports = db;
