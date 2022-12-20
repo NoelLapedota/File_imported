49f9f8de8c425760148066d5c983c8c226a838ee
Mrleon123 <Mrleon@hotmail.it>
2022-12-20 13:02:34 +0100

    first commit

--- connectMySQL.js
+++ connectMySQL.js
@@ -3,18 +3,15 @@ const mysql = require('mysql2/promise')
 
 async function db() {
   return await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "fileImportDb2",
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
         
   });
 }
 
 
-// host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
+
 
 module.exports = db;
